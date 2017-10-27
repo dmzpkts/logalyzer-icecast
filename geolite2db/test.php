61b82a7b49238ced400b9d34597cf45a1455f951
Hunter Perrin <hperrin@gmail.com>
2017-10-26 18:12:56 -0700

    Added GeoLite2 db test file.

---
+++ geolite2db/test.php
@@ -0,0 +1,30 @@
+<?php
+
+// https://dev.maxmind.com/geoip/geoip2/geolite2/
+
+require '../vendor/autoload.php';
+
+use MaxMind\Db\Reader;
+
+$ipAddress = '8.8.8.8';
+$databaseFile = 'GeoLite2-City.mmdb';
+
+$reader = new Reader($databaseFile);
+
+$ipInfo = $reader->get($ipAddress);
+
+$returnInfo = [
+  'timeZone' => @$ipInfo['location']['time_zone'],
+  'continentCode' => @$ipInfo['continent']['code'],
+  'continent' => @$ipInfo['continent']['names']['en'],
+  'countryCode' => @$ipInfo['country']['iso_code'],
+  'country' => @$ipInfo['country']['names']['en'],
+  'provinceCode' => @$ipInfo['subdivisions'][0]['iso_code'],
+  'province' => @$ipInfo['subdivisions'][0]['names']['en'],
+  'postalCode' => @$ipInfo['postal']['code'],
+  'city' => @$ipInfo['city']['names']['en'],
+];
+
+print_r($returnInfo);
+
+$reader->close();
